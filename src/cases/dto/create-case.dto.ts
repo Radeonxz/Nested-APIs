a1132ebf6d7b1cdee6bf474fc875db2194849b0f
Radeonxz <xuanxxzhao@gmail.com>
2020-04-21 22:48:17 -0400

    feat: add product-api

--- src/cases/dto/create-case.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateCaseDto {
-  readonly name: string;
-  readonly description: string;
-  readonly clients: number;
-}
--- src/config/keys.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  mongoURI: 'MONGODB_URI',
-};
