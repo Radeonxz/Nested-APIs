2ba99e9fd0e34e5d39860c105de79d15730c0726
Radeonxz <xuanxxzhao@gmail.com>
2020-05-27 23:40:37 -0400

    chore: update service

--- item-api/src/items/items.service.ts
+++ item-api/src/items/items.service.ts
@@ -3,20 +3,7 @@ import { Item } from './interfaces/item.interface';
 
 @Injectable()
 export class ItemsService {
-  private readonly items: Item[] = [
-    {
-      id: '123456789',
-      name: 'Item 1',
-      description: 'This is item 1',
-      qty: 100,
-    },
-    {
-      id: '234567890',
-      name: 'Item 2',
-      description: 'This is item 2',
-      qty: 200,
-    },
-  ];
+  private readonly items: Item[] = [];
 
   findAll(): Item[] {
     return this.items;
