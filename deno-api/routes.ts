9f20693cbc50f09cd0753a62caef432fc6f511bf
Radeonxz <xuanxxzhao@gmail.com>
2020-08-26 22:15:57 -0400

    chore: add mongodb and routes

---
+++ deno-api/routes.ts
@@ -0,0 +1,10 @@
+import { RouterContext } from "https://deno.land/x/oak/mod.ts";
+import db from "./mongodb.ts";
+
+const notesCollection = db.collection("notes");
+
+const getNotes = async (ctx: RouterContext) => {
+  const notes = await notesCollection.find();
+  ctx.response.body = notes;
+};
+export { getNotes };
