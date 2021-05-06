17e184b805db15dcf01af6c27c9ae37cdff76084
Radeonxz <xuanxxzhao@gmail.com>
2021-05-05 22:35:16 -0400

    chore: add es awaits and browser api

---
+++ deno-api/deno-basics/01_features/05_browser_api/fetch.ts
@@ -0,0 +1,5 @@
+// Using fetch()
+const todo = await fetch(
+  "https://jsonplaceholder.typicode.com/todos/1"
+).then((response) => response.json());
+console.log("TODO from jsonplaceholder ", todo);
