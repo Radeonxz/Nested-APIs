c61a27fb3a4ab7fc977c3c5930a0e99c51a2cbb1
Radeonxz <xuanxxzhao@gmail.com>
2021-05-28 23:58:28 -0400

    chore: add db

--- deno-api/oak-api/server.ts
+++ deno-api/oak-api/server.ts
@@ -3,8 +3,35 @@ import {
   Router,
   RouterContext
 } from "https://deno.land/x/oak/mod.ts";
+import { DataTypes, Database, Model } from "https://deno.land/x/denodb/mod.ts";
 
 import postsRouter from "./routes/posts";
+
+const connection = new Database({
+  host: "localhost",
+  username: "postgres",
+  password: "root",
+  database: "deno-api"
+});
+
+const db = new Database(connection);
+
+class Post extends Model {
+  static table = "posts";
+  static timestamps = true;
+
+  static fields = {
+    id: { primaryKey: true, autoIncrement: true },
+    username: DataTypes.STRING,
+    body: DataTypes.STRING
+  };
+
+  static defaults = {};
+}
+
+db.link([Post]);
+
+await db.sync({ drop: true });
 
 const app = new Application();
 const router = new Router();
